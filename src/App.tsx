8882e80fc5ec93c6d5844f9455dd3cd3982bb52a
boxy8 <zou.emily@yahoo.co.nz>
2024-04-14 00:25:32 +1200

    feat: style input and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ function App() {
         ))
       }
     </div>
-      <h1 className={styles.counter}>Completed: {completedChores}</h1>
+      <h2 className={styles.counter}>Completed: {completedChores}</h2>
     </div>
   )
 }
